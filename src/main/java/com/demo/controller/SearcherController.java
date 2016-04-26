89ccaa70197b7f9fc80e79f5ce62df80709cd20b
igor <mail@mail.pl>
2016-04-26 12:19:43 +0200

    26_04/2

---
+++ src/main/java/com/demo/controller/SearcherController.java
@@ -0,0 +1,39 @@
+package com.demo.controller;
+
+import com.demo.service.CityService;
+import com.demo.service.FacultyService;
+import com.demo.service.SchoolService;
+import com.demo.service.SchoolTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class SearcherController {
+
+
+    @Autowired
+    private SchoolTypeService schoolTypeService;
+    @Autowired
+    private CityService cityService;
+    @Autowired
+    private SchoolService schoolService;
+    @Autowired
+    private FacultyService facultyService;
+
+
+
+    @RequestMapping(value = "/searcher", method = RequestMethod.GET)
+    public String searcher (Model model) {
+        model.addAttribute("schools", schoolService.getSchools());
+        model.addAttribute("schoolTypes", schoolTypeService.getSchoolTypes());
+        model.addAttribute("faculties", facultyService.findFaculties());
+        model.addAttribute("cities", cityService.getCities());
+        return "searcher";
+    }
+
+
+}
